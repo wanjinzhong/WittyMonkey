d35d43ff72ddb187408e75166f2ec7d914fccef8
neilw <neilw@synnex.org>
2017-01-19 14:08:30 +0800

    邮件发送incoding

---
+++ WittyMonkey/src/main/java/com/wittymonkey/util/SendEmail.java
@@ -0,0 +1,70 @@
+package com.wittymonkey.util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class SendEmail {
+	public static void sendValidateCode(String address){
+		final Properties props = new Properties();
+		try {
+			props.load(ClassLoader.class.getResourceAsStream("/mail.properties"));
+			Session session = Session.getDefaultInstance(props);
+			session.setDebug(true);
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(props.getProperty("address")));
+			msg.setRecipients(Message.RecipientType.TO,InternetAddress.parse(
+					"1051750377@qq.com"));
+			msg.setSubject("测试邮件");
+			msg.setText("这是一条测试邮件,本次验证码：" + getValidateCode());
+			Transport transport = session.getTransport("smtp");
+			// smtp验证，就是你用来发邮件的邮箱用户名密码
+			transport.connect(props.getProperty("smtp"), props.getProperty("address"), props.getProperty("password"));
+			// 发送
+			transport.sendMessage(msg, msg.getAllRecipients());
+			transport.close();
+			/*HtmlEmail email = new HtmlEmail();
+			email.setHostName(props.getProperty("smtp"));
+			email.setCharset(props.getProperty("encoding"));
+			email.addTo(address);
+			email.setFrom(props.getProperty("address"), props.getProperty("name"));
+			email.setAuthentication(props.getProperty("address"), props.getProperty("password"));
+			email.setSubject("测试邮件");
+			email.setMsg("这是一条测试邮件,本次验证码：" + getValidateCode());
+			email.send();*/
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		System.out.println("邮件发送成功");
+	}
+	private static String getValidateCode(){
+		StringBuffer buffer = new StringBuffer();
+		for(int i = 0; i < 6; i ++){
+			buffer.append((int)(Math.random()*10));
+		}
+		return buffer.toString();
+	}
+}
---
+++ WittyMonkey/src/main/java/com/wittymonkey/vo/MimeMessage.java
@@ -0,0 +1,52 @@
+package com.wittymonkey.vo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MimeMessage {
+	private String sender;
+	private List<String> recipient = new ArrayList<String>();
+	private List<String> cc = new ArrayList<String>();
+	private String subject;
+	private String content;
+	private Date sendTime;
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public List<String> getRecipient() {
+		return recipient;
+	}
+	public void setRecipient(List<String> recipient) {
+		this.recipient = recipient;
+	}
+	public List<String> getCc() {
+		return cc;
+	}
+	public void setCc(List<String> cc) {
+		this.cc = cc;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getSendTime() {
+		return sendTime;
+	}
+	public void setSendTime(Date sendTime) {
+		this.sendTime = sendTime;
+	}
+	
+	
+}
