0ef20f698b4f37c00e6d694a8477e901b9bfc1d2
neilw <neilw@synnex.org>
2017-04-12 11:27:51 +0800

    upload get_next_staff_no.sql

--- Document/mysql/get_next_staff_no.sql
+++ Document/mysql/get_next_staff_no.sql
@@ -1,28 +1,4 @@
-/*
-SQLyog Ultimate v11.25 (64 bit)
-MySQL - 5.7.16-log : Database - wittymonkey
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`wittymonkey` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `wittymonkey`;
-
-/* Procedure structure for procedure `get_next_staff_id` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `get_next_staff_id` */;
-
-DELIMITER $$
-
-/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_next_staff_id`(
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_next_staff_id`(
 		IN hotelId INT)
 BEGIN
 DECLARE curr_no INT;
@@ -61,10 +37,4 @@ ELSE
 	END IF;
 END IF;
 SELECT staffNo AS staff_no;
-END */$$
-DELIMITER ;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+END
